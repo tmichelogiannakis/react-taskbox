f6634ea3cc12296c7e7bb7b6a13accfc5b03d880
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2020-08-31 23:42:04 +0300

    feat: add generated static Storybook

---
+++ docs/precache-manifest.b18cd533cb385daccbed5f8a40b3e867.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b16047549d1feb3105c96e110e79040",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.0fddfe7fa16f73eabee2.bundle.js"
+  },
+  {
+    "url": "runtime~main.0fddfe7fa16f73eabee2.bundle.js"
+  },
+  {
+    "revision": "dc52ef60b8025b88c9e2",
+    "url": "static/css/main.fbdfeca7.chunk.css"
+  },
+  {
+    "url": "vendors~main.0fddfe7fa16f73eabee2.bundle.js"
+  },
+  {
+    "url": "vendors~main.0fddfe7fa16f73eabee2.bundle.js.LICENSE.txt"
+  }
+]);
